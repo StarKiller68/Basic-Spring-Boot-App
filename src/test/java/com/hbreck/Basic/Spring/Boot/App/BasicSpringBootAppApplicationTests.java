1da9ca5b46da780d6c31386016a8b103f1f659ad
hbreck <hbreckenridge@yahoo.com>
2023-07-27 14:46:25 -0400

    First commit

---
+++ src/test/java/com/hbreck/Basic/Spring/Boot/App/BasicSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbreck.Basic.Spring.Boot.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
